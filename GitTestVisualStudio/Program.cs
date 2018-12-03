279ea86b472e94bcc8e1f5dade6d64c272b54254
Sasha <alex.sikilinda@gmail.com>
2018-12-03 20:48:10 +0200

    Add project files.

---
+++ GitTestVisualStudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTestVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
